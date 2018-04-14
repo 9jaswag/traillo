3fd9e574038c0bcdfdf10871e7e2b55c35aa4164
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-14 21:13:06 +0100

    - generate index controller - setup home component

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Home extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home
