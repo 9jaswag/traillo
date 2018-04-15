3c5d5eb74e4988d9acd9550a33b7822e174b4d4f
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-15 21:53:09 +0100

    - generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/login'
+
   root 'index#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
