1af44e217ad01fd285eb559c65ccd42b262c4e1a
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-15 16:06:04 +0100

    - break homepage into component - prerender homepage

---
+++ app/javascript/components/ExternalHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default () => (
+  <header>
+    <nav className="navbar navbar-expand-sm navbar-light sticky-top" style={{ backgroundColor: '#0079bf' }}>
+      <a className="navbar-brand" href="/">
+        <img src="logo.png" alt="Trailo logo" id="logo" />
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="mobile-menu">
+        <div className="navbar-nav ml-md-auto d-md-flex">
+          <button className="btn btn-dark" type="button">Login</button>
+          <button className="btn btn-success ml-3" type="button">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  </header>
+);
+
