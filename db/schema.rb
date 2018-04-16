2805c86fde7839ab181d3c4c41307c810ab88bfa
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-16 10:54:43 +0100

    - recreate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415204914) do
+ActiveRecord::Schema.define(version: 20180416095108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,16 @@ ActiveRecord::Schema.define(version: 20180415204914) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "reset_digest"
+    t.datetime "reset_time"
+    t.string "activation_digest"
+    t.datetime "activation_time"
+    t.boolean "activated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
     t.index ["username"], name: "index_users_on_username"
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,12 @@ FactoryBot.define do
     bio "MyText"
     avatar ""
     username "MyString"
+    email "MyString"
+    password_digest "MyString"
+    reset_digest "MyString"
+    reset_time "2018-04-16 10:51:09"
+    activation_digest "MyString"
+    activation_time "2018-04-16 10:51:09"
+    activated false
   end
 end
