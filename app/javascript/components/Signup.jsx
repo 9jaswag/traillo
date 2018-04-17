2e0cdb7658370cd8539b5a4835d9dfa7c5b95eab
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-17 12:38:37 +0100

    - implement routing with react router

---
+++ app/javascript/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Signup extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Signup
