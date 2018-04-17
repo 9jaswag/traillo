6ca7b75b63b7503c7d3f93e342f59dbba4523150
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-17 15:14:25 +0100

    - style signup page

--- app/javascript/components/HomePageContent.jsx
+++ app/javascript/components/HomePageContent.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from 'react-router-dom';
 
 export default () => (
   <div className="jumbotron jumbotron-fluid mb-0 bg-transparent home-hero">
@@ -9,7 +10,9 @@ export default () => (
       <hr className="my-4" />
       <p>You're one click away from organizing and prioritizing your project.</p>
       <p className="lead">
-        <a className="btn btn-success btn-lg button__external" href="#" role="button">Sign Up - It's Free</a>
+        <Link className="btn btn-success btn-lg button__external" to="/signup" role="button">
+          Sign Up - It's Free
+        </Link>
       </p>
       <p>Already use Traillo? <a href="" className="home-hero-link">Log in</a>.</p>
     </div>
