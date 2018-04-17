6c75d41573a44089a27e8daad3a0cf9f4677f7ea
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-17 21:21:38 +0100

    - make buttons and inut text components

---
+++ app/javascript/components/TextInput.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// TextInput.propTypes = {
+//   icon: PropTypes.string,
+//   type: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   value: PropTypes.string.isRequired,
+//   label: PropTypes.string,
+//   placeholder: PropTypes.string,
+//   onChange: PropTypes.func.isRequired,
+//   onBlur: PropTypes.func,
+//   onFocus: PropTypes.func,
+//   error: PropTypes.string
+// };
+
+export default ({ type, name, value, label, placeholder,
+  onChange, helpId, helpText, required, error, onBlur, onFocus }) => (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        className="form-control form-control-lg"
+        id={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        autoComplete="off"
+        required={required}
+      />
+      <small id={helpId} className="form-text text-muted">{helpText}</small>
+    </div>
+  );
