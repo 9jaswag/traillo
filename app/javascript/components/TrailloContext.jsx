7fe243d41caae09a6e816d271cb0066a6d5384bf
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-18 12:43:37 +0100

    - implement ContextAPI

--- app/javascript/components/TrailloContext.jsx
+++ app/javascript/components/TrailloContext.jsx
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { createContext } from "react"
 import PropTypes from "prop-types"
+
+// Initialize a context
+const Context = createContext();
+
+export const { Provider, Consumer } = Context
 class TrailloContext extends React.Component {
-  render () {
+  state = {
+    auth: {},
+    value: "I'm balling"
+  }
+  actions = {
+    doStuff: () => this.setState({ value: 'I"ve been changed' })
+  }
+  render() {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <Provider value={{
+        state: this.state,
+        actions: this.actions
+      }}>
+        {this.props.children}
+      </Provider>
     );
   }
 }
