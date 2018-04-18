f6a2edbe05101536e5a1d7fd61293bbfade32bc6
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-18 18:29:47 +0100

    - fix routing reload issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/activate/:token', to: 'users#edit', as: 'activate'
-  get '/login', to: 'users#index'
-  post '/login', to: 'users#login'
+  scope '/api' do
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
+    get '/activate/:token', to: 'users#edit', as: 'activate'
+    get '/login', to: 'users#index'
+    post '/login', to: 'users#login'
+  end
 
   get 'users/show'
 
 
   root 'index#index'
+  get "/*path" => "index#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
