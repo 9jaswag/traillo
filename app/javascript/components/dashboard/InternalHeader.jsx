ccff7ce637820bfa1fad169edf40823e7a9ea322
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-22 23:33:12 +0100

    - get header board dropdown data via props

--- app/javascript/components/dashboard/InternalHeader.jsx
+++ app/javascript/components/dashboard/InternalHeader.jsx
@@ -36,14 +36,19 @@ export default () => (
           <input className="form-control form-control-sm board-dropdown-search-input col-12" type="text" placeholder="Find boards by name..." autoFocus />
         </form>
         <div className=''>
-          <div className="col-s-12 board-dropdown-section position-relative p-3">
+          <div className="col-12 board-dropdown-section position-relative p-3">
             <i className="far fa-star text-muted"></i>
             <span className="section-title ml-1 text-muted">Starred Boards</span>
             <i className="fas fa-minus float-right pr-3 text-muted position-absolute"></i>
           </div>
           <ul className="board-dropdown-projects mt-2">
-            <HeaderBoardDropdown />
+            <HeaderBoardDropdown
+              url='/'
+              bkgImg='https://trello-backgrounds.s3.amazonaws.com/SharedBackground/480x480/56d4e601aae022ee7d2c0ce4d04dd372/photo-1519552928909-67ca7aef9265.jpg'
+              name='Badass Project'
+            />
           </ul>
+          <div className="col-12"></div>
         </div>
       </div>
     </div>
