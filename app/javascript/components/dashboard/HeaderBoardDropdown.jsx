de56d89f1adcb7bc9ae5e9b6d7b8f77db89ef2a9
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-22 23:33:12 +0100

    - get header board dropdown data via props

--- app/javascript/components/dashboard/HeaderBoardDropdown.jsx
+++ app/javascript/components/dashboard/HeaderBoardDropdown.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export default () => (
+export default ({ url, bkgImg, name }) => (
   <li className="dropdown-project">
     <div className="dropdown-project-wrapper position-relative">
-      <span className="dropdown-project-background"></span>
+      <span className="dropdown-project-background" style={{ backgroundImage: `url(${bkgImg})` }}></span>
       <span className="compact-board-tile-fade"></span>
-      <Link to='/' className="dropdown-project-link">
-        <span className="dropdown-project-link-thumbnail"></span>
+      <Link to={url} className="dropdown-project-link">
+        <span className="dropdown-project-link-thumbnail" style={{ backgroundImage: `url(${bkgImg})` }}></span>
         <span className="dropdown-project-link-text-detail">
-          <span className="dropdown-project-link-text-detail-name">Project Bench</span>
+          <span className="dropdown-project-link-text-detail-name">{name}</span>
         </span>
       </Link>
     </div>
