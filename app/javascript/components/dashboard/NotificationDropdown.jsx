4f778bdbbea7a116c48f74e5d4ce3c2f9ad0f8fa
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-23 19:09:19 +0100

    - create notification dropdown

---
+++ app/javascript/components/dashboard/NotificationDropdown.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default () => (
+  <div className="collapse col-12 p-0" id="notification-dropdown">
+    <div className="col-12 p-0">
+      <div className="col-12 text-center py-1"><span className="text-muted">Notifications</span></div>
+      <hr className='m-0' />
+      <div className="col-12 text-center py-5">
+        <span className="text-muted">No Notifications</span>
+      </div>
+    </div>
+  </div>
+);
