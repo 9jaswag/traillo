a9d578653f7886410a1b02b9844063ce8106fb6d
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-25 19:23:48 +0100

    - rename javascript bundle filename - remove dummy html page - change bundle name in react-on-rails settings

--- app/javascript/packs/traillo-bundle.js
+++ app/javascript/packs/traillo-bundle.js
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
+import App from '../components/App.jsx';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  App
 });
