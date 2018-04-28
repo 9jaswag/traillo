ca1d9bbee2329adf0c077994cc36942d59fc725f
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-04-28 12:00:00 +0100

    Revert "- rename javascript bundle filename - remove dummy html page - change bundle name in react-on-rails settings"
    This reverts commit cca2b2217eda555eeccaffc25475a4bc43ea2612.

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails';
 
-import App from '../components/App.jsx';
+import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  App
+  HelloWorld,
 });
