8526e9312790544616d1e247511a70265adc387c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-01 12:54:55 +0100

    - build user-board association when creating board

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :user_boards
+  has_many :boards, through: :user_boards
   has_secure_password
 
   class << self
