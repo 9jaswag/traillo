86c40345dfabd587061491fbb4f9bd849a331b52
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-03 21:43:11 +0100

    - create card

--- app/javascript/components/board/BoardList.jsx
+++ app/javascript/components/board/BoardList.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { inject, observer } from "mobx-react";
 
-class BoardList extends Component {
+@inject('store')
+@observer class BoardList extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      newCardName: ''
+    }
 
     this.showCreateCardForm = this.showCreateCardForm.bind(this);
     this.hideAddCardDropdown = this.hideAddCardDropdown.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onSubmit(event) {
     event.preventDefault();
+    const name = this.state.newCardName;
+    const { createCard } = this.props.store.Board;
+    if (name.length < 1) {
+      console.log('please enter card name')
+      return;
+    }
+    createCard({ 
+      name,
+      list_id: this.props.list.id
+     })
+     this.setState({ newCardName: '' });
+  }
+
+  onChange(event) {
+    this.setState({ newCardName: event.target.value })
   }
 
   showCreateCardForm(event) {
@@ -27,6 +49,9 @@ class BoardList extends Component {
     const form = event.target.nextElementSibling;
     form.classList.remove('collapse');
     form.firstElementChild.firstElementChild.firstElementChild.value = openFormValue;
+    this.setState({
+      newCardName: openFormValue
+    })
     event.target.classList.add('collapse')
   }
 
@@ -35,6 +60,7 @@ class BoardList extends Component {
     const form = button.closest("div div");
     form.classList.add('collapse');
     form.previousElementSibling.classList.remove('collapse');
+    this.setState({ newCardName: '' })
   }
 
   render() {
@@ -65,7 +91,7 @@ class BoardList extends Component {
           <div className='add-card-wrapper collapse'>
             <form id='add-card-form position-relative' action="" onSubmit={this.onSubmit}>
               <div className="card-composer p-2 m-2">
-                <textarea name="" id="" className="card-compose-textarea" required></textarea>
+                <textarea name="" id="" className="card-compose-textarea" value={this.state.newCardName} onChange={this.onChange} required></textarea>
               </div>
               <button className='btn btn-success btn-sm mx-2 mb-2' type='submit'>Add</button>
               <i className="ion-close-round position-absolute ml-3 add-card-close-icon" onClick={this.hideAddCardDropdown}></i>
