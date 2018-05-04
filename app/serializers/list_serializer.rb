11884c2eb8fdf32d29bda40257d82f7c21cbaccd
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-04 11:42:53 +0100

    - display cards in lists

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,6 +1,10 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :cards
 
   belongs_to :board
   has_many :cards
+
+  def cards
+    object.cards
+  end
 end
