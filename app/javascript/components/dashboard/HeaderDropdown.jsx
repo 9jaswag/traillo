6c7b77c1b13fb707e19843c3b2df8a75c4606f53
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-04 13:22:22 +0100

    - add boards ho header dropdown

--- app/javascript/components/dashboard/HeaderDropdown.jsx
+++ app/javascript/components/dashboard/HeaderDropdown.jsx
@@ -2,30 +2,55 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import HeaderDropdownBoard from './HeaderDropdownBoard';
+import { inject, observer } from "mobx-react";
 
-export default ({ focus }) => (
-  <div className="collapse" id="board-dropdown">
-    <div className="p-1 col-7 col-sm-4 col-md-3 col-lg-3">
-      <form className="form-inline my-2 my-md-0 ml-1">
-        <input className="form-control form-control-sm board-dropdown-search-input col-12" type="text" placeholder="Find boards by name..." ref={focus} />
-      </form>
-      <div className=''>
-        <div className="col-12 board-dropdown-section position-relative p-3">
-          <i className="far fa-star text-muted"></i>
-          <span className="section-title ml-1 text-muted">Starred Boards</span>
-          <i className="fas fa-minus float-right pr-3 text-muted position-absolute"></i>
-        </div>
-        <ul className="board-dropdown-projects mt-2">
-          <HeaderDropdownBoard
-            url='/'
-            bkgImg='https://trello-backgrounds.s3.amazonaws.com/SharedBackground/480x480/56d4e601aae022ee7d2c0ce4d04dd372/photo-1519552928909-67ca7aef9265.jpg'
-            name='Badass Project'
-          />
-        </ul>
-        <div className="">
-          <button className="btn btn-light btn-sm btn-block text-left" data-toggle="modal" data-target="#createBoardModal">Create new board...</button>
+@inject('store')
+@observer class HeaderDropdown extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    const { getUserBoards, userBoards } = this.props.store.Dashboard;
+    getUserBoards();
+  }
+
+  render() {
+    const { focus } = this.props
+    const { userBoards } = this.props.store.Dashboard;
+    const boards = userBoards.map(board => (
+      <HeaderDropdownBoard
+        key={board.id}
+        url={`/board/${board.uid}/${board.name.replace(/ /g, '-').toLocaleLowerCase()}`}
+        bgImg={board.bg_img}
+        bgColor={board.bg_color}
+        name={board.name}
+      />
+    ));
+    return (
+      <div className="collapse" id="board-dropdown">
+        <div className="p-1 col-7 col-sm-4 col-md-3 col-lg-3">
+          <form className="form-inline my-2 my-md-0 ml-1">
+            <input className="form-control form-control-sm board-dropdown-search-input col-12" type="text" placeholder="Find boards by name..." ref={focus} />
+          </form>
+          <div className=''>
+            <div className="col-12 board-dropdown-section position-relative p-3">
+              <i className="far fa-star text-muted"></i>
+              <span className="section-title ml-1 text-muted">Personal Boards</span>
+              <i className="fas fa-minus float-right pr-3 text-muted position-absolute"></i>
+            </div>
+            <ul className="board-dropdown-projects mt-2">
+              {userBoards.length > 0 && boards}
+            </ul>
+            <div className="">
+              <button className="btn btn-light btn-sm btn-block text-left" data-toggle="modal" data-target="#createBoardModal">Create new board...</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+export default HeaderDropdown;
+
--- app/javascript/components/dashboard/HeaderDropdownBoard.jsx
+++ app/javascript/components/dashboard/HeaderDropdownBoard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export default ({ url, bkgImg, name }) => (
+export default ({ url, bgImg, bgColor, name }) => (
   <li className="dropdown-project">
     <div className="dropdown-project-wrapper position-relative">
-      <span className="dropdown-project-background" style={{ backgroundImage: `url(${bkgImg})` }}></span>
+      <span className="dropdown-project-background" style={{ backgroundImage: `url(${bgImg})`, backgroundColor: `${bgColor}` }}></span>
       <span className="compact-board-tile-fade"></span>
       <Link to={url} className="dropdown-project-link">
-        <span className="dropdown-project-link-thumbnail" style={{ backgroundImage: `url(${bkgImg})` }}></span>
+        <span className="dropdown-project-link-thumbnail" style={{ backgroundImage: `url(${bgImg})` }}></span>
         <span className="dropdown-project-link-text-detail">
           <span className="dropdown-project-link-text-detail-name">{name}</span>
         </span>
