f51775b63f1c1c5fcb29010677b7d8fbd248fb6d
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-05 21:10:43 +0100

    - create task modal left side - handle toggling of edit desc form

--- app/javascript/components/board/ProjectPage.jsx
+++ app/javascript/components/board/ProjectPage.jsx
@@ -18,6 +18,7 @@ import BoardList from './BoardList';
     this.showAddListDropdown = this.showAddListDropdown.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.toggleDescriptionEditForm = this.toggleDescriptionEditForm.bind(this);
   }
 
   componentWillMount() {
@@ -78,6 +79,13 @@ import BoardList from './BoardList';
     formWrapper.classList.remove('focused');
     submitButton.classList.remove('show');
     closeButton.classList.remove('show');
+  }
+
+  toggleDescriptionEditForm() {
+    const button = document.querySelector('.show-desc-edit');
+    const editForm = document.querySelector('.card-detail-edit');
+    button.classList.toggle('collapse');
+    editForm.classList.toggle('show');
   }
 
   render() {
@@ -155,7 +163,7 @@ import BoardList from './BoardList';
             <div className="modal-content">
               <div className="modal-header pb-0">
                 <i className="far fa-credit-card text-muted pr-2"></i>
-                <h6 className="modal-title" id="exampleModalLongTitle">Card Title</h6>
+                <h6 className="modal-title task-modal-title" id="exampleModalLongTitle">Card Title</h6>
                 <div className="current-list">
                   <small>in list List Name</small>
                 </div>
@@ -167,17 +175,23 @@ import BoardList from './BoardList';
                 <div className="container-fluid p-0">
                   <div className="row">
                     <div className="col-12 col-sm-8 col-lg-9 left">
-                      <section className="card-detail-edit position-relative">
+                      <p className='mb-0'>
+                        <button className="show-desc-edit quiet-button btn-block text-left" onClick={this.toggleDescriptionEditForm}>
+                          <i className="far fa-edit pr-1"></i>
+                          Edit description...
+                        </button>
+                      </p>
+                      <section className="card-detail-edit position-relative collapse">
                         <form action="" className="inline">
                           <textarea name="" id="card-details-textarea" className='col-12 p-2' placeholder="Add a more detailed description…" required></textarea>
                           <button className="btn btn-success btn-sm" type='submit'>Save</button>
-                          <i className="ion-close-round position-absolute ml-3 card-detail-close-icon"></i>
+                          <i className="ion-close-round position-absolute ml-3 card-detail-close-icon" onClick={this.toggleDescriptionEditForm}></i>
                         </form>
                       </section>
                       <section className="card-comment-section mt-3">
                         <div>
                           <i className="far fa-comment text-muted d-inline-block"></i>
-                          <h6 className='d-inline-block pl-2'>Add Comments</h6>
+                          <h6 className='d-inline-block pl-2 task-modal-title'>Add Comments</h6>
                         </div>
                         <div className="new-comment position-relative">
                           <div className="member">
@@ -212,7 +226,7 @@ import BoardList from './BoardList';
                       <section className="card-comment-section activity-section mt-3">
                         <div>
                           <i className="fas fa-list text-muted d-inline-block"></i>
-                          <h6 className='d-inline-block pl-2'>Activity</h6>
+                          <h6 className='d-inline-block pl-2 task-modal-title'>Activity</h6>
                         </div>
                         <div className="new-comment position-relative">
                           <div className="member">
@@ -233,7 +247,38 @@ import BoardList from './BoardList';
                         </div>
                       </section>
                     </div>
-                    <div className="col-12 col-sm-4 col-lg-3">right</div>
+                    <div className="col-12 col-sm-4 col-lg-3">
+                      <h6 className='task-modal-title'>Add</h6>
+                      <div className="add-buttons">
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="far fa-user pr-1"></i> Members
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="ion-android-checkbox-outline pr-1"></i> Checklist
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="far fa-clock pr-1"></i> Due Date
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="fas fa-paperclip pr-1"></i> Attachment
+                        </button>
+                      </div>
+                      <h6 className='task-modal-title mt-4'>Action</h6>
+                      <div className="add-buttons">
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="fas fa-arrow-right pr-1"></i> Move
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="far fa-credit-card pr-1"></i> Copy
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="fas fa-eye pr-1"></i> Watch
+                        </button>
+                        <button type="button" className="btn btn-secondary btn-block add-btn btn-sm">
+                          <i className="fas fa-trash pr-1"></i> Archive
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
