ae24c14ca23e975a056c51ec1f3dd1c5a661cc8c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-10 12:57:16 +0100

    - toggle checkbox based on is_done status - set default is_done to false

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
+  before_save :set_is_done
   validates :name, presence: true
 
   belongs_to :checklist
+
+  def set_is_done
+    self.is_done = false if self.is_done.nil?
+  end
 end
