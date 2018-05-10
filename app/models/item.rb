3e419282027e5cc42b16ff556a428ffc04a418f0
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-10 10:42:33 +0100

    - refactor item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+
+  belongs_to :checklist
 end
--- db/migrate/20180510094016_create_items.rb
+++ db/migrate/20180510094016_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.string :name
       t.boolean :is_done
+      t.references :checklist, foreign_key: true
 
       t.timestamps
     end
