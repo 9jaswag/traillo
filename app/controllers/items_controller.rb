62750f7a619899cfdd544099780c09bd74aabac5
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-10 16:36:46 +0100

    - update items by toggling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,19 @@ class ItemsController < ApplicationController
     render json: checklist if item.save!
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update!(update_params)
+    render json: item, status: :ok
+  end
+
+  private
+
   def item_params
     params.permit(:name, :checklist_id)
+  end
+
+  def update_params
+    params.permit(:name, :checklist_id, :id, :is_done)
   end
 end
--- app/javascript/components/board/Checklist.jsx
+++ app/javascript/components/board/Checklist.jsx
@@ -11,7 +11,8 @@ import classnames from 'classnames';
     }
 
     this.checkCheckbox = this.checkCheckbox.bind(this);
-    this.toggleAddItemForm = this.toggleAddItemForm.bind(this);
+    this.showAddItemForm = this.showAddItemForm.bind(this);
+    this.hideAllAddItemForms = this.hideAllAddItemForms.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onChange = this.onChange.bind(this);
   }
@@ -33,41 +34,70 @@ import classnames from 'classnames';
       checklist_id: this.props.checklist.id
     })
     this.setState({ name: '' })
-    this.toggleAddItemForm();
+    this.hideAllAddItemForms();
   }
 
-  toggleAddItemForm() {
-    const showButton = document.querySelector('.show-add-item');
-    const form = document.querySelector('.add-item-form');
-    showButton.classList.toggle('collapse');
-    form.classList.toggle('show');
+  showAddItemForm(event) {
+    this.hideAllAddItemForms()
+    const showButton = event.target;
+    const form = event.target.nextElementSibling
+    showButton.classList.add('collapse');
+    form.classList.add('show');
+  }
+
+  hideAllAddItemForms() {
+    const showButton = document.querySelectorAll('.show-add-item');
+    const form = document.querySelectorAll('.add-item-form');
+    showButton.forEach((button, index) => {
+      button.classList.remove('collapse');
+      form[index].classList.remove('show')
+    })
+
+    // hide all forms recursively: params, array of forms, index(0), class to remove
+    // if (array.length == (index + 1)) {
+    //   array[index].classList.remove(removeClass);
+    //   return;
+    // } else {
+    //   array[index].classList.remove(removeClass);
+    //   return this.hideAllAddItemForms(array, (index + 1))
+    // }
   }
 
   checkCheckbox(event) {
     const checkbox = event.target;
     const text = checkbox.parentElement.nextElementSibling.firstElementChild.firstElementChild;
+    const { updateItem } = this.props.store.Board;
+    let is_done;
     if (checkbox.classList.contains('fa-square')) {
-      event.target.classList.remove('fa-square')
-      event.target.classList.add('fa-check-square', 'checked')
-      text.classList.add('underline')
+      event.target.classList.remove('fa-square');
+      event.target.classList.add('fa-check-square', 'checked');
+      text.classList.add('underline');
+      is_done = true;
     } else {
       event.target.classList.add('fa-square')
       event.target.classList.remove('fa-check-square', 'checked')
       text.classList.remove('underline')
+      is_done = false
     }
+    updateItem({
+      id: event.target.dataset.id,
+      checklist_id: this.props.checklist.id,
+      is_done,
+      name: text.textContent
+    })
   }
 
   render() {
     const { checklist } = this.props;
     const { items } = checklist;
-    console.log(items)
+    // console.log(items)
     const checklistItems = items.map(item => (
       <div className="checklist-item px-1" key={item.id}>
         <div className="checklist-item-checkbox d-inline-block">
           <i className={classnames('far text-muted', {
             'fa-check-square': item.is_done,
             'fa-square': !item.is_done
-          })} onClick={this.checkCheckbox}></i>
+          })} data-id={item.id} onClick={this.checkCheckbox}></i>
         </div>
         <div className="checklist-item-details d-inline-block pl-2">
           <div className="checklist-item-name">
@@ -91,7 +121,7 @@ import classnames from 'classnames';
           </div>
         </div>
         {items.length > 0 && checklistItems}
-        <button className="quiet-button btn-block text-left show-add-item" onClick={this.toggleAddItemForm}>
+        <button className="quiet-button btn-block text-left show-add-item" onClick={this.showAddItemForm}>
           Add an item...
         </button>
         <div className="add-item-form collapse">
@@ -105,7 +135,7 @@ import classnames from 'classnames';
               onChange={this.onChange}
               required></textarea>
             <button className='btn btn-sm btn-success'>Add</button>
-            <i className="ion-close-round position-absolute ml-3 add-item-close-icon" onClick={this.toggleAddItemForm}></i>
+            <i className="ion-close-round position-absolute ml-3 add-item-close-icon" onClick={this.hideAllAddItemForms}></i>
           </form>
         </div>
       </section>
